86b3ca6681ae37c751b49fbd33c7160c59623eaa
ARHR2123 <137183048+ARHR2123@users.noreply.github.com>
2024-06-17 18:07:36 -0500

    update_CRUD

--- bdd/borrar.js
+++ bdd/borrar.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc, getDocs, query, updateDoc, where } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc, getDocs, query, updateDoc, where } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,13 +9,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBJrg9Alv7KvoQ9PJhmbCw1el03gRa1sVM",
-    authDomain: "reserva-3caaa.firebaseapp.com",
-    projectId: "reserva-3caaa",
-    storageBucket: "reserva-3caaa.appspot.com",
-    messagingSenderId: "652924412002",
-    appId: "1:652924412002:web:df0c04cdf836197113f13b",
-    measurementId: "G-NR8K69TGLP"
+    apiKey: "AIzaSyB7xd9KIfsqffWxf8-F8ZUeKMcHugspqbc",
+    authDomain: "registrovue-66e54.firebaseapp.com",
+    projectId: "registrovue-66e54",
+    storageBucket: "registrovue-66e54.appspot.com",
+    messagingSenderId: "783449294180",
+    appId: "1:783449294180:web:e5f8f5dcc9a7ed6472a671"
 };
 
 // Initialize Firebase
