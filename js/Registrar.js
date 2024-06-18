824913aca745c6781f0db74964568e61dab02a64
ARHR2123 <137183048+ARHR2123@users.noreply.github.com>
2024-06-17 22:11:25 -0500

    update_redes

--- js/Registrar.js
+++ js/Registrar.js
@@ -1,23 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
-    getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword,
-    GoogleAuthProvider, signInWithPopup, FacebookAuthProvider, sendEmailVerification, OAuthProvider,
-    GithubAuthProvider
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+    getAuth, createUserWithEmailAndPassword, sendEmailVerification
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBJrg9Alv7KvoQ9PJhmbCw1el03gRa1sVM",
-    authDomain: "reserva-3caaa.firebaseapp.com",
-    projectId: "reserva-3caaa",
-    storageBucket: "reserva-3caaa.appspot.com",
-    messagingSenderId: "652924412002",
-    appId: "1:652924412002:web:df0c04cdf836197113f13b",
-    measurementId: "G-NR8K69TGLP"
+    apiKey: "AIzaSyB7xd9KIfsqffWxf8-F8ZUeKMcHugspqbc",
+    authDomain: "registrovue-66e54.firebaseapp.com",
+    projectId: "registrovue-66e54",
+    storageBucket: "registrovue-66e54.appspot.com",
+    messagingSenderId: "783449294180",
+    appId: "1:783449294180:web:e5f8f5dcc9a7ed6472a671"
 };
 
 // Initialize Firebase
