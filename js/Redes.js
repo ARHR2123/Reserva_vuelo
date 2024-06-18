529c5256eeecb137b47250f0b92b5a35f459c69f
ARHR2123 <137183048+ARHR2123@users.noreply.github.com>
2024-06-17 22:29:16 -0500

    update_redes2

--- js/Redes.js
+++ js/Redes.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
-    getAuth,
-    GoogleAuthProvider, signInWithPopup, FacebookAuthProvider, OAuthProvider,
+    getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword,
+    GoogleAuthProvider, signInWithPopup, FacebookAuthProvider, sendEmailVerification, OAuthProvider,
     GithubAuthProvider
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
